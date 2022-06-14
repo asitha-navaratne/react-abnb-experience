df71a401ab8d61d455d39f9cc814f95b98a5281d
Asitha Navaratne <asithan476@gmail.com>
2022-06-14 21:47:34 +0530

    Made Card component dynamic

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,19 +1,22 @@
-import image from "../images/katie-zaferes.png";
 import star from "../images/Star 1.png";
 
-function Card() {
+function Card(props) {
   return (
     <div className="card">
-      <img src={image} alt="card" className="card--image" />
+      <img
+        src={require(`../images/${props.img}`)}
+        alt="card"
+        className="card--image"
+      />
       <div className="stats">
         <img src={star} alt="star" className="card--star" />
-        <span>5.0</span>
-        <span className="grey">(6) &#x2022; </span>
-        <span className="grey">USA</span>
+        <span>{props.rating}</span>
+        <span className="grey">({props.reviewCount}) &#x2022; </span>
+        <span className="grey">{props.country}</span>
       </div>
-      <p>Life lessons with Katie Zaferes</p>
+      <p>{props.title}</p>
       <p>
-        <span className="bold">From $136</span> / person
+        <span className="bold">From ${props.price}</span> / person
       </p>
     </div>
   );
